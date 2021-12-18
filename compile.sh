be14fb0496f4d0492947643eebf5945089aaa02e
BrokenXZ <74031662+BrokenXZ@users.noreply.github.com>
2021-12-17 22:09:17 -0500

    add building for 32 bit windows

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-gcc -s gaycalc.c -o gaycalc && x86_64-w64-mingw32-gcc -o gaycalc.exe gaycalc.c
+gcc -s gaycalc.c -o gaycalc && x86_64-w64-mingw32-gcc -o gaycalc.exe gaycalc.c && i686-w64-mingw32-gcc -o gaycalc_32bit.exe gaycalc.c
